58c082f2edccc25547839a01cf2ac81814ddfc41
Hugo Gruson <hugo.gruson@protonmail.com>
2021-11-26 15:55:29 +0100

    Add epinow2 model as submodule

--- data-submissions/epinow2/main.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-TODAY=$(date +'%Y-%m-%d')
-wget -P data-submissions/epinow2 https://raw.githubusercontent.com/epiforecasts/europe-covid-forecast/master/submissions/rt-forecasts/$TODAY/$TODAY-epiforecasts-EpiNow2.csv
